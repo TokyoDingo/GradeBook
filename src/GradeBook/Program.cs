52ad7de290938e0047844a00de5325858bdf7665
Tom Hemming <TomHem@marvin.com>
2019-10-03 19:21:01 -0500

    Controlling the flow of execution

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -7,17 +7,47 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            var gradeBook = new Book( "Tom's Grade Book ");
+            Console.WriteLine("Enter the title of your grade book.");
+            var name = Console.ReadLine();
+
+            var gradeBook = new Book( name );
             var Statistics = new Statistics();
-            gradeBook.AddGrade( 89.1 );
-            gradeBook.AddGrade( 90.5 );
-            gradeBook.AddGrade( 77.5 );
+
+            while(true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit");
+                var input = Console.ReadLine();
+
+                if(input == "q")
+                {
+                    break;
+                }
+
+                try{
+                    var grade = double.Parse(input);
+                    gradeBook.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
             Statistics = gradeBook.GetStatistics();
 
-            //Console.WriteLine( "Statics for " + name );
+            Console.WriteLine( "Statics for " + name );
             Console.WriteLine( "    Lowest grade is " + Statistics.Low );
             Console.WriteLine( "    Highest grade is " + Statistics.High );
             Console.WriteLine( "    Average grade is " + Statistics.Average ); 
+            Console.WriteLine( "    Letter grade is " + Statistics.Letter ); 
             
         }
     }
--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -9,18 +9,130 @@ namespace GradeBook.Tests
         public void BookCalculatesAnAverageGrade()
         {            
             // arrange
-            var book = new Book( "" );
-            book.AddGrade( 89.1 );
-            book.AddGrade( 90.5 );
-            book.AddGrade( 77.3 );
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
 
             // act
             Statistics result = book.GetStatistics();
 
             // assert
-            Assert.Equal( 85.6, result.Average, 1 );
-            Assert.Equal( 90.5, result.High, 1 );
-            Assert.Equal( 77.3, result.Low, 1 );            
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);      
+            Assert.Equal('B', result.Letter);      
+        }
+        
+        [Fact]
+        public void GradeMustBeGreaterThanZero()
+        {            
+            var book = new Book("");
+            var errorWasThrown = false;
+
+            try
+            {
+                book.AddGrade(-1);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                errorWasThrown = true;
+            }
+
+            Assert.True(errorWasThrown);            
+        }
+        
+        [Fact]
+        public void GradeMustBeLessThan100()
+        {            
+            var book = new Book("");
+            var errorWasThrown = false;
+
+            try
+            {
+                book.AddGrade(105);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                errorWasThrown = true;
+            }
+
+            Assert.True(errorWasThrown);            
+        }
+
+        [Fact]
+        public void LetterGradeIsValid()
+        {
+            var book = new Book("");
+            var errorWasThrown = false;
+
+            try{
+                book.AddLetterGrade('E');
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex);
+                errorWasThrown = true;
+            }
+
+            Assert.True(errorWasThrown);
+        }        
+
+        [Fact]
+        public void LetterGradeAIs90()
+        {
+            var book = new Book("");
+
+            book.AddLetterGrade('A');
+            var stat = book.GetStatistics();
+
+            Assert.Equal(90, stat.High);
+        }      
+
+        [Fact]
+        public void LetterGradeBIs80()
+        {
+            var book = new Book("");
+
+            book.AddLetterGrade('B');
+            var stat = book.GetStatistics();
+
+            Assert.Equal(80, stat.High);
+        }
+
+        [Fact]
+        public void LetterGradeCIs70()
+        {
+            var book = new Book("");
+
+            book.AddLetterGrade('C');
+            var stat = book.GetStatistics();
+
+            Assert.Equal(70, stat.High);
+        }
+
+        [Fact]
+        public void LetterGradeDIs60()
+        {
+            var book = new Book("");
+
+            book.AddLetterGrade('D');
+            var stat = book.GetStatistics();
+
+            Assert.Equal(60, stat.High);
+        }
+
+        [Fact]
+        public void LetterGradeFIsZero()
+        {
+            var book = new Book("");
+
+            book.AddLetterGrade('F');
+            var stat = book.GetStatistics();
+
+            Assert.Equal(0, stat.High);
         }
     }
 }
